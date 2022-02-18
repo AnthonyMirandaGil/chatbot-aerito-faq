96e36642b8d672a7158a2b12f8fbdc341ef281f5
Anthony Miranda <anthonymirandagil@gmail.com>
2022-02-18 13:08:14 -0500

    web chatbot

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# chatbot-aerito-faq
+# Chatbot Aerito
+
+## Descripción
+
+Aerito es un chatbot creado para la atención automatizada por la pagina web a las consultas frecuentes de los alumnos de la Facultad de Ciencias sobre los temas de matricula, procedimientos y tramites académicos.
+
+## Descripción general de los archivos
+
+Los siguientes archivos son propios del framework RASA
+
+`data/nlu.yml` - contiene los ejemplos de consultas que son proveídas como datos y son clasificación en una intención que determinara la siguiente acción o respuesta del chatbot.
+
+`data/stories.yml` - contiene los flujos de ejemplos en los que el chatbot sera entrenado.
+
+`domain.yml` - contiene las respuestas del chatbot
+
+`config.yml` - configuración del pipeline de procesamiento natural para procesar las entradas del usuario
+
+La arquitectura del proyecto se ha generado con el Framework de código abierto RASA, puede encontrar mas información sobre la descripción de estos archivos en la [documentación oficial](https://rasa.com/docs/rasa/training-data-format).
+
+`webchat/` - contiene el widget del chat que puedo ser integrado al pagina web para la interaction con el bot.
+
+`despligue/` - contiene archivos para el despliegue por docker en un servidor.
+
+## Instalación Local
+
+Para instalar, clone el repositorio o descomprima el archivo zip, luego ejecute los siguiente comando para instalar las dependencias
+
+```
+pip3 install requiriments.txt
+pip3 install rasa[spacy] rasa[transformers]
+python -m spacy download es_core_news_md
+```
+
+Esto instalará el bot y todos sus requisitos. Tenga en cuenta que este bot debe usarse con Python 3.6 o 3.7.
+
+# Como ejecutar Aerito
+
+Si desea volver a entrenar el modelo luego de hacer cambios o agregar mas datos corra el comando `rasa train` para entrenar el modelo
+
+Luego para compilar y ejecutar el modelo chatbot primero ejecute el siguiente comando:
+
+```
+rasa run --cors "*"
+```
+
+Luego, corra los siguientes comando para iniciar el chat web de la aplicación.
+
+```
+cd webchat
+python3 -m http.server
+```
--- diagnostic/diagnostic_data.py
+++ diagnostic/diagnostic_data.py
@@ -6,9 +6,10 @@ from rasa.shared.nlu.constants import TEXT
 from rasa.shared.constants import DIAGNOSTIC_DATA
 import pathlib
 import numpy as np
+import json
 
-YOUR_RASA_MODEL_DIRECTORY = './models'
-YOUR_RASA_MODEL_NAME = '20220105-180228'
+YOUR_RASA_MODEL_DIRECTORY = '../models'
+YOUR_RASA_MODEL_NAME = '20220212-220728'
 
 def load_interpreter(model_dir, model):
     path_str = str(pathlib.Path(model_dir) / model)
@@ -17,32 +18,46 @@ def load_interpreter(model_dir, model):
     _, nlu_model = get_model_subdirectories(model_path)
     return Interpreter.load(nlu_model)
 
-
 if __name__ == "__main__":
     interpreter = load_interpreter(YOUR_RASA_MODEL_DIRECTORY, f"{YOUR_RASA_MODEL_NAME}.tar.gz")
     data = interpreter.default_output_attributes()
-    data[TEXT] = "Hola como estas"
+    data[TEXT] = "cual es el formato o solicitud?"
+    #data[TEXT] = "no te entiendo"
     message = Message(data=data)
     print(interpreter.pipeline)
     for i , e in enumerate(interpreter.pipeline):
         e.process(message)
-        featurizer = e
-        if i ==2:
-            break
-    tokens = message.get('text_tokens')
-    print('message:', message.as_dict_nlu())
-    word_vectors = np.array([featurizer.model.get_word_vector(t.text) for t in tokens])
-    print('word_vectors',word_vectors)
-    #nlu_diagnostic_data = message.as_dict()[DIAGNOSTIC_DATA]
+    #    featurizer = e
+    #    if i ==2:
+    #        break
+    #tokens = message.get('text_tokens')
+    #print('message:', message.as_dict_nlu())
+    #word_vectors = np.array([featurizer.model.get_word_vector(t.text) for t in tokens])
+    #print('word_vectors',word_vectors)
+    print('DIAGNOSTIC_DATA', DIAGNOSTIC_DATA)
+    dict_nlu = message.as_dict()
+    print('------------------------------------------')
+    print("dict_nlu keys:", dict_nlu.keys())
+    dict_out = { 
+        'text': dict_nlu['text'], 
+        'intent': dict_nlu['intent'], 
+        'intent_ranking': dict_nlu['intent_ranking'],
+        'response_selector' : dict_nlu['response_selector']}
+    
+    with open('out.json','w') as f:
+        json.dump( dict_out ,f)
+    print('dict_out: ', dict_out)
+    #print('dict_nlu:', dict_nlu)
     #print('message.as_dict_nlu()',  message.as_dict_nlu())
-    #print('DIAGNOSTIC_DATA',DIAGNOSTIC_DATA)
+    #nlu_diagnostic_data = message.as_dict_nlu()[DIAGNOSTIC_DATA]
+    #
     #print('text_tokens', message.as_dict_nlu()['text_tokens'])
     #for component_name, diagnostic_data in nlu_diagnostic_data.items():
-        #print(diagnostic_data.keys())
-        #print(f"attention_weights for {component_name}:")
+    #    print(diagnostic_data.keys())
+    #    print(f"attention_weights for {component_name}:")
     #    attention_weights = diagnostic_data["attention_weights"]
-        #print(attention_weights)
+    #    print(attention_weights)
 
-        #print(f"\ntext_transformed for {component_name}:")
-     #   text_transformed = diagnostic_data["text_transformed"]
-        #print(text_transformed)
+    #    print(f"\ntext_transformed for {component_name}:")
+    #    text_transformed = diagnostic_data["text_transformed"]
+    #    print(text_transformed)
