945c6465064df00d574b22f5359dbdf08f88d09f
anthony <anthonymirandagil@gmail.com>
2022-06-30 18:45:11 -0500

    [update] migrate rasav3

--- db/db.py
+++ db/db.py
@@ -38,11 +38,6 @@ def getCoursesAndCodes():
     return cursos
 
 
-def getCourses():
-    df = pd.read_csv('db/courses.csv')
-    allCourses = df['courses'].values.tolist()
-    return allCourses
-
 
 def getCourseSchedule(course_code, section):
     conn = getConnection()
@@ -86,19 +81,6 @@ def getCourseRoom(course_code, section):
     curso_rooms = cursor.fetchall()
     return curso_rooms
 
-def getCourseSchedule2(course, section):
-    return [{
-            'day': 'Lunes',
-            'start': '8pm',
-            'end': '10am',
-            'type': 'Teoria'},
-            {
-            'day': 'Martes',
-            'start': '2pm',
-            'end': '4pm',
-            'type': 'Practica'}]
-
-
 def getSectionsCourseByCode(code):
     conn = getConnection()
     cursor = conn.cursor(cursor_factory=extras.RealDictCursor)
@@ -113,35 +95,6 @@ def getSectionsCourseByCode(code):
     return sections
 
 
-def getSectionsCourse(course):
-    df = pd.read_excel('db/Insumos Matrícula SIGA 2022-1 (1).xlsx',
-                       sheet_name='HorariosConfiguracionCursoSecci')
-    df['NOMBRE DEL CURSO'] = df['NOMBRE DEL CURSO'].apply(lambda x: x.lower())
-    sections = df[df['NOMBRE DEL CURSO'] ==
-                  course.lower()]['SECCION'].values.tolist()
-    sections = list(set(sections))
-    sections.sort()
-    return sections
-
-
-aulas = ['J3-102','J3 122','J3-202', 'R1 460']
-
-j3_aulas = [ aula for aula in aulas if re.search('^J', aula, re.IGNORECASE) ]
-first_floor_j3 = [j3_aula for j3_aula in j3_aulas if re.search('^(J3(\s|\-)?1)', j3_aula, re.IGNORECASE)]
-        
-print(j3_aulas)
-
-print(first_floor_j3)
-
-r_aulas = [ aula for aula in aulas if re.search('^R\d?', aula, re.IGNORECASE) ]
-
-floors_r = [
-                        [
-                        r_aula for r_aula in r_aulas 
-                            if re.search(f'^(R\d(\s|\-)?{i})', r_aula, re.IGNORECASE)
-                        ] for i in range(1,5)
-                    ] 
-print(floors_r)
 #sections = getSectionsCourseByCode('BEF01')
 #print(sections)
 #slot_value = 'algoritmos'
